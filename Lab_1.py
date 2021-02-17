e2d9689a9f5e0c8d4288c358bd109c9c29a4cd89
vEagle13 <78980659+vEagle13@users.noreply.github.com>
2021-02-17 11:51:14 +0200

    Update Lab_1.py

--- Lab_1.py
+++ Lab_1.py
@@ -7,25 +7,11 @@ pen.speed("fastest")
 pen.hideturtle()
 pen.color("white")
 screen.bgcolor("black")
-turtle.listen()
 
 def f(p):
     x = (2 + 7*math.cos(math.sin(p)+math.sin(121*p)))*math.cos(p)
     y = (2 + 7*math.cos(math.sin(p)+math.sin(121*p)))*math.sin(p)
     return x,y
-
-def event(x,y):
-    pen.undo()
-    pen.penup()
-    pen.setpos(200,200)
-    pen.color("red")
-    theta = math.atan(y/x)
-    if x<0:
-        theta = theta + math.pi
-    if y<0 and x>0:
-        theta = theta + math.pi*2
-    pen.write(str(f(theta)) + "\ntheta = " + str(theta))
-    pass
 
 #основна проргама
 scale = 20
@@ -35,6 +21,6 @@ while theta <= math.tau:
     x,y = f(theta)
     pen.goto(x*scale,y*scale)
     pen.down()
-    theta = theta + 0.005
-screen.onscreenclick(event,3)
+    theta = theta + 0.001
+
 turtle.mainloop()
